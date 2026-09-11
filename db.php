035d9ffd46df55849015f328ccfab76197958e38
Sabelo Branden Mkhonto <sabelobranden@gmail.com>
2026-09-11 11:42:13 +0200

    refix

--- db.php
+++ db.php
@@ -1,14 +1,20 @@
 <?php
-$servername = "127.0.0.1";
-$username = "Police_Application";
-$password = "Spearchirp@20";
-$dbname = "police_application";
 
+$config = require __DIR__ . '/config/database.php';
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$servername = $config['host'];
+$username = $config['username'];
+$password = $config['password'];
+$dbname = $config['database'];
 
+$conn = new mysqli(
+    $servername,
+    $username,
+    $password,
+    $dbname
+);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Database connection failed.");
 }
 ?>
